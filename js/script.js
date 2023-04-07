d046e311375207bbd4b0d4c3f081a5be1d2e1cdd
uvnb195 <41299590+uvnb195@users.noreply.github.com>
2023-04-07 17:30:49 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,10 @@ var swiper = new Swiper(".slide-content", {
     },
 
   });
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarNav')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
