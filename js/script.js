efb0b4af9af9d4f0aa50c35829155a2fb8f9448c
uvnb195 <41299590+uvnb195@users.noreply.github.com>
2023-04-07 21:27:16 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
+    freeMode: true,
     centerSlide: 'true',
     fade: 'true',
     grabCursor: 'true',
